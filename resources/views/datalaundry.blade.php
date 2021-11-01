047929aee1099de6a04d1c27080cbff1d6ae7de2
yunuswidodo <younews372@gmail.com>
2021-11-02 00:18:17 +0700

    edit

--- resources/views/datalaundry.blade.php
+++ resources/views/datalaundry.blade.php
@@ -44,7 +44,7 @@
                         <span class="text-secondary text-xs font-weight-bold">{{$d->tgl_laundry}}</span>
                       </td>
                       <td class="align-middle">
-                        <a class="btn btn-link text-danger text-gradient px-3 mb-0" href="javascript:;"><i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete</a>
+                        <a class="btn btn-link text-warning text-gradient px-3 mb-0" href="{{url('editdata', [$d->id])}}"><i class="fas fa-edit"></i> Detail</a>
                       </td>
                       @endforeach
                   </tbody>
